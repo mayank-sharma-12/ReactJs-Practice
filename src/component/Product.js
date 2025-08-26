56303ac7a38fcca2650f2ffe4f0aab2e2405f722
Mayank Sharma <mayank.consurgys@gmail.com>
2025-08-26 12:51:15 +0530

    Successfully learned about Array Props and Rendering

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function Product({productTitle, productPrice}) {
+function Product({productTitle, productPrice, feature}) {
+    const options = feature.map((feature) => <li>{feature}</li>);
     return ( 
         <>
         <h1>{productTitle}</h1>
-        <h5>{productPrice}</h5>
+        <h5>Price : {productPrice}</h5>
+        <p>Features</p>
+        <p>{options}</p>
         </>
      );
 }
--- src/component/ProductsTab.js
+++ src/component/ProductsTab.js
@@ -2,11 +2,12 @@ import React from "react";
 import Product from "./Product";
 
 function ProductTab() {
+    const options = ["HiTech", "Tech"];
   return (
     <>
-      <Product productTitle='Laptop' productPrice={30000} />
-      <Product productTitle='Mobile' productPrice={15000} />
-      <Product productTitle='Tablet' productPrice={45000} />
+      <Product productTitle='Laptop' productPrice={30000} feature={options} />
+      <Product productTitle='Mobile' productPrice={15000} feature={options} />
+      <Product productTitle='Tablet' productPrice={45000} feature={options} />
     </>
   );
 }
