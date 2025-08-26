242ba6f60c597f6d95439ddb954dc93bb7082e8f
Mayank Sharma <mayank.consurgys@gmail.com>
2025-08-26 14:37:29 +0530

    Successfully Learned About Dynamic Component Styling

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,13 +1,15 @@
 import React from 'react'
-
+import "./Product.css";
 function Product({productTitle, productPrice, feature}) {
+    const isDiscount = productPrice > 30000;
+    const styles = {backgroundColor : isDiscount ? 'yellow' : ""}
     return ( 
-        <>
+        <div className='Product' style={styles}>
         <h1>{productTitle}</h1>
         <h5>Price : {productPrice}</h5>
         {productPrice > 15000 ? <p>Discount of 10%</p> : null}
         {productPrice > 35000 && <p>Discount of 15%</p>}
-        </>
+        </div>
      );
 }
 
